e7a46d1d8c8b1837ab59e0799b341c1a9863fb3b
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-12-22 12:00:01 +0700

    fix: update deploy.yml branch formatting and add env_file to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "sep-discord-bot",
       script: "src/index.js",
       watch: false,
+      env_file: ".env",
       env: {
         NODE_ENV: "production",
       },
